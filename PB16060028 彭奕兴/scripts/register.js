3f563c0ff94f127c951e46a83a117aa9b58b47a0
pyxaws <37150855+pyxaws@users.noreply.github.com>
2018-07-06 16:06:39 +0800

    Add files via upload

---
+++ PB16060028 彭奕兴/scripts/register.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function(){
+    $(".description1").click(function(){
+        $(this).attr("class","description2");
+        $(".description3").click();
+    });
+    $(".description3").click(function(){
+        $(this).attr("class","description4")
+    });
+    var e=jQuery.Event("click");
+    $(".logobutton").click(function(){
+        $(".description1").trigger(e);
+    });
+});
+function logoonclick(obj) {
+    obj.className='logobutton1';
+    obj.nextSibling.className='description2';
+}
+jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
